45381f11f4064e7ea15a5c88c8603d82f5506820
lejames <okejames55@gmail.com>
2022-06-16 16:39:00 +0300

    Creating a tweet

--- python_tweepy.py
+++ python_tweepy.py
@@ -5,16 +5,22 @@ consumer_key = config("CONSUMER_KEY")
 consumer_secret = config("CONSUMER_SECRET")
 access_token = config("ACCESS_TOKEN")
 access_token_secret = config("ACCESS_SECRET")
+bearer_token = config("BEARER_TOKEN")
 
 # Authenticate to twitter
-auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-auth.set_access_token(access_token, access_token_secret)
+# auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+# auth.set_access_token(access_token, access_token_secret)
 
-# create API object
-api = tweepy.API(auth)
+# # create API object
+# api = tweepy.API(auth)
 
-try:
-    api.verify_credentials()
-    print("Authentication Okay")
-except:
-    print("Error occured")
+# try:
+#     api.verify_credentials()
+#     print("Authentication Okay")
+# except:
+#     print("Error occured")
+
+client = tweepy.Client(bearer_token, consumer_key, consumer_secret, access_token, access_token_secret)
+
+# first tweet
+client.create_tweet(text="Hello Twitter")
