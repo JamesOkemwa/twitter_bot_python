29ce001dce76437780d0a4dc38165b4ccb7b1a12
lejames <okejames55@gmail.com>
2022-06-16 16:34:06 +0300

    credentials verification

--- python_tweepy.py
+++ python_tweepy.py
@@ -3,15 +3,18 @@ from decouple import config
 
 consumer_key = config("CONSUMER_KEY")
 consumer_secret = config("CONSUMER_SECRET")
-access_token = config("ACCESS_KEY")
-access_secret = config("ACCESS_SECRET")
+access_token = config("ACCESS_TOKEN")
+access_token_secret = config("ACCESS_SECRET")
 
 # Authenticate to twitter
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-auth.set_access_token(access_token, access_secret)
+auth.set_access_token(access_token, access_token_secret)
 
 # create API object
 api = tweepy.API(auth)
 
-# create a tweet
-api.update_status("Hello Tweepy")
+try:
+    api.verify_credentials()
+    print("Authentication Okay")
+except:
+    print("Error occured")
