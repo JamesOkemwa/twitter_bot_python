4152d36437740f975a45a7ab87a9d74e881a3a03
lejames <okejames55@gmail.com>
2022-06-20 10:42:46 +0300

    Streaming tweets that match a particular criteria

--- python_tweepy.py
+++ python_tweepy.py
@@ -1,4 +1,5 @@
 import tweepy
+import time
 from decouple import config 
 
 consumer_key = config("CONSUMER_KEY")
@@ -29,6 +30,31 @@ api = tweepy.API(auth)
 #     print(tweet.text)
 
 # fetching a particular user's tweets
-person = client.get_user(username="TechbroJ").data.id
-for tweet in client.get_users_tweets(person).data:
-    print(tweet.text)
+# person = client.get_user(username="TechbroJ").data.id
+# for tweet in client.get_users_tweets(person).data:
+#     print(tweet.text)
+
+# Tweepy stream -> Getting tweets as they're being posted
+search_terms = ["python", "programming", "coding"]
+
+class MyStream(tweepy.StreamingClient):
+    def on_connect(self):
+        """Called after the Streaming API has connected successfully"""
+        print("Connected")
+
+    def on_tweet(self, tweet):
+        """Called when the API detects a tweet that meets the criteria"""
+        if tweet.referenced_tweets == None:
+            print(tweet.text)
+
+            # time.sleep(0.2)
+
+
+stream = MyStream(bearer_token=bearer_token)
+
+for term in search_terms:
+    stream.add_rules(tweepy.StreamRule(term))
+
+stream.filter(tweet_fields=["referenced_tweets"])
+
+        
